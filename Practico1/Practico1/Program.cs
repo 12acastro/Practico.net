85d88dd8941e37a833cd2690c996c885563c23a5
12acastro <88839179+12acastro@users.noreply.github.com>
2022-08-18 22:53:18 -0300

    Practico1

---
+++ Practico1/Practico1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Practico1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona [] personas = new Persona[20];
+            Console.WriteLine("Mi Primera App");
+            int i=0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Nueva Persona");
+                    Persona per = new Persona();
+                    Console.WriteLine("Nombre: ");
+                    per.Nombre = Console.ReadLine();
+                    Console.WriteLine("Apellido: ");
+                    per.Apellido = Console.ReadLine();
+                    Console.WriteLine("Documento: ");
+                    per.Documento = Console.ReadLine();
+                    Console.WriteLine("Fecha Nacimiento: ");
+                    Console.WriteLine("Dia: ");
+                    string dia = Console.ReadLine();
+                    Console.WriteLine("Mes: ");
+                    string mes = Console.ReadLine();
+                    Console.WriteLine("Anio: ");
+                    string anio = Console.ReadLine();
+                    per.FechaNacimiento = new DateTime(Int32.Parse(anio), Int32.Parse(mes), Int32.Parse(dia)); 
+                    
+
+
+                    Console.WriteLine("Persona");
+                    Console.WriteLine("     Nombre: " + per.Nombre);
+                    Console.WriteLine("     Documento: " + per.Documento);
+                    
+                    personas[i] = per;
+                    i++;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("ERROR: "+ ex.Message);
+                }
+            }
+
+            while (!Console.ReadLine().Equals("EXIT"));
+
+            for(int e =0; e<i; e++)
+            {
+                for(int j = i - 1;j>=e; j--)
+                {
+                    if (personas[j - 1].FechaNacimiento > personas[j].FechaNacimiento){
+                        Persona p = personas[j - 1];
+                        personas[j - 1] = personas[j];
+                        personas[j] = p;
+                    }
+                }
+            }
+
+            for (int l = 0; l < i; l++)
+            {
+                Console.WriteLine(personas[l].Nombre + " " + personas[l].Apellido +" FN: " + personas[l].FechaNacimiento);
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Practico1/Shared/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    // internal class Persona
+    public class Persona
+    {
+        public string Nombre { get; set; } = "-- Sin Nombre --";
+
+        private string documento = "";
+        public string Documento
+        { 
+            get { return documento; } 
+            set {
+                if (value.Length < 7)
+                    throw new Exception("Formato de documento incorrecto.");
+                else
+                    documento = value.ToUpper();
+                } 
+        
+
+        }
+        public string Apellido = "-- Sin Apellido --";
+
+        public DateTime FechaNacimiento;
+
+    }
+}
